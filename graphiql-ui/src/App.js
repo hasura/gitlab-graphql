1babb69e0b03fb391e812c6ca3d64c5719a4653c
Karthik Venkateswaran <karthik@hasura.io>
2018-07-12 18:36:41 +0530

    Removes architecture image

--- graphiql-ui/src/App.js
+++ graphiql-ui/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import GraphiQL from 'graphiql';
 import 'graphiql/graphiql.css'
 import './App.css';
+
 const baseDomain = 'http://178.128.180.80';
 
 const graphqlUrl = `${baseDomain}:8090/v1alpha1/graphql`;
@@ -10,6 +11,24 @@ class App extends Component {
   state = {
     cookie: null,
     includeCookie: true
+  }
+  constructor() {
+    super();
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+
+    this.state = {
+      show: false
+    };
+  }
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
   }
 
   componentDidMount() {
@@ -67,9 +86,12 @@ class App extends Component {
     const demoImage = require("./assets/gitlab-graphql-demo.png");
 
     return (
-      <div className="App">
+      <div id="app" className="App">
         <div className="Banner">
           {`This GraphiQL uses your gitlab cookie to authenticate the requests. If you are not logged in, `} <a href={baseDomain}> please login here. </a>
+        </div>
+        <div className="ArchitectureText">
+          This demo connects gitlab postgres using Hasura GraphQL engine. You can checkout the architecture <a href="/ui/architecture.png">here</a>.
         </div>
         <h3 className="Header">Architecture</h3>
         <div className="Description">
